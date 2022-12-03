059e6003f5b88743432853a20ea2f3b0d4ed38c2
Debdut karpas <nilkarpas@gmail.com>
2022-12-04 00:07:23 +0530

    updated backend server

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const history = require('connect-history-api-fallback');
+const { response } = require('express');
 
 const generateRandomString = length => {
     let text = '';
@@ -53,10 +54,9 @@ if(cluster.isMaster){
     //Prority server any static files.
     app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-    app
-        .use(express.cors())
-        .use(express.cookieParser())
-        .use(
+    app.use(cors());
+    app.use(cookieParser())
+    app.use(
             history({
                 verbose : true,
                 rewrites: [
@@ -71,7 +71,48 @@ if(cluster.isMaster){
         res.render(path.resolve(__dirname, '../client/build/index.html'));
     });
 
-    
+    app.get('/login',function (req,res){
+        const state = generateRandomString(16);
+        res.cookie(stateKey,state)
+
+        // your application requests authorization
+        const scope =
+            'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public';
+
+        res.redirect(
+            `https://accounts.spotify.com/authorize?${querystring.stringify({
+                response_type: 'code',
+                client_id : CLIENT_ID,
+                scope : scope,
+                redirect_uri : REDIRECT_URI,
+                state : state,
+            })}`
+        );
+    });    
+
+    app.get('/refresh_token', function(req,res){
+        const refresh_token = req.query.refresh_token;
+        const authOptions = {
+            url : 'https://accounts.spotify.com/api/token',
+            headers : {
+                Authorization : `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+                    'base64'
+                )}`
+            },
+            form : {
+                grant_type : 'refresh_token',
+                refresh_token,
+            },
+            json : true,
+        };
+
+        request.post(authOptions, function (error, response, body){
+            if(!error && response.statusCode === 28){
+                const access_token = body.access_token;
+                res.send({ access_token });
+            }
+        });
+    });
 
     app.listen(PORT, function() {
         console.warn(`Node cluster worker ${process.pid} : listening on port :${PORT}`);
