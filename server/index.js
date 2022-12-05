1f9f9078ffdb593f1f6a11c939e245b55a7c19c8
Debdut karpas <nilkarpas@gmail.com>
2022-12-05 23:18:52 +0530

    updating backend

--- server/index.js
+++ server/index.js
@@ -68,6 +68,7 @@ if(cluster.isMaster){
         )
     
     app.get('/', function (req, res) {
+        console.log('reaching')
         res.render(path.resolve(__dirname, '../client/build/index.html'));
     });
 
@@ -88,7 +89,53 @@ if(cluster.isMaster){
                 state : state,
             })}`
         );
-    });    
+    });  
+
+    app.get('/callback', function (req, res) {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+      res.redirect(`/#${querystring.stringify({ error: 'state_mismatch' })}`);
+    } else {
+      res.clearCookie(stateKey);
+      const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: REDIRECT_URI,
+          grant_type: 'authorization_code',
+        },
+        headers: {
+          Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+            'base64',
+          )}`,
+        },
+        json: true,
+      };
+
+      request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const access_token = body.access_token;
+          const refresh_token = body.refresh_token;
+
+          // we can also pass the token to the browser to make requests from there
+          res.redirect(
+            `${FRONTEND_URI}/#${querystring.stringify({
+              access_token,
+              refresh_token,
+            })}`,
+          );
+        } else {
+          res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+        }
+      });
+    }
+  });  
 
     app.get('/refresh_token', function(req,res){
         const refresh_token = req.query.refresh_token;
